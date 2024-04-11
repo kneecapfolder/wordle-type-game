0a981723620126b22b76ca105b314e3f5d5b37d0
kneecapFolder <ilan7gr@gmail.com>
2024-04-11 13:34:19 +0300

    finished the typing system prototype

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import { useState } from 'react';
 
 
 function App() {
+    const [word, setWord] = useState('BREAK')
+    const [placed, setPlaced] = useState('');
     const [current, setCurrent] = useState('');
 
-    onkeydown = (e) => {
+    onkeydown = async (e) => {
 
         // Check if the key is a letter
         if (current.length < 5 && e.key.length === 1 && /^[a-zA-Z]+$/.test(e.key))
@@ -15,14 +17,20 @@ function App() {
         else if (e.key === 'Backspace')
             setCurrent(current.slice(0, -1))
         else if (current.length === 5 && e.key === 'Enter') {
-            setCurrent('');
+            // check if word exists
+            const req = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${current}`);
+            if (req.ok) {    
+                setPlaced(placed+current);
+                setCurrent('');
+            }
+            else console.log('Not a valid word')
         }
     }
 
     return (
         <>
             <h1>wordle</h1>
-            <Board current={current}></Board>
+            <Board word={word} placed={placed}>{current}</Board>
         </>
     );
 }
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,14 +1,43 @@
 
 interface Props {
-    current: string;
+    children: string;
+    placed: string;
+    word: string;
 }
 
-function Board({ current }: Props) {
+function Board({ children, placed, word }: Props) {
+    const placedArr = placed.match(/.{5}/g);
+
     return (
         <div id="board">
-            {current.split('').map((elm, index) => (
-                <div key={index} className="key">{elm}</div>
-            ))}
+            {
+                placedArr?.map(subArr =>
+                    subArr.split('').map((ch, index) =>
+                        <div key={index} className={
+                            word[index] === ch ? 'key correct' :
+                            word.includes(ch) ? 'key close' :
+                            'key false'
+                        }>
+                            {ch}
+                        </div>
+                    )
+                )
+            }
+            
+            {
+                children.split('').map((elm, index) => (
+                    <div key={index} className="key">
+                        {elm}
+                    </div>
+                ))
+            }
+            
+            {
+                Array.from({length:30-children.length-placed.length},
+                    (x, index) => <div className="key" key={index} />
+                )
+            }
+
         </div>
     );
 }
